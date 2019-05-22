330434d93a807e56ab84b387dbc6c5d50c2eccce
Kayode Olumo <Kayode.dev@gmail.com>
2019-05-22 20:07:16 +0100

    GS-Site-Codebase
    Entire Codebase

---
+++ projects/myenvironment/globalshoe/shop/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Category,Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
+# Create your views here.
+
+def index(request):
+    text_var = 'This is my first django app web page.'
+    return HttpResponse(text_var)
+
+#Category views
+def allProdCat(request, c_slug = None):
+    c_page = None
+    products_lists = None
+    if c_slug!= None:
+        c_page = get_object_or_404(Category,slug = c_slug)
+        products_lists = Product.objects.filter(category = c_page, available = True)
+    else:
+        products_lists = Product.objects.all().filter(available = True)
+    # Paginator code
+    paginator = Paginator(products_lists, 15)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'shop/category.html',{'category':c_page,'products':products})
+
+def ProdCatDetail(request, c_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=c_slug, slug=product_slug)
+    except Exception as e:
+        raise
+    return render(request,'shop/product.html', {'product':product})
